35b8739ef58879900e9dd57beea51efe0f4f9219
Banitongwa David <davidbanitongwa@gmail.com>
2023-07-08 21:57:09 +0100

    coming soon component

--- pages/layout.js
+++ pages/layout.js
@@ -1,15 +1,18 @@
-import { Inter } from 'next/font/google'
-import Head from 'next/head'
+import { Inter } from "next/font/google";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <>
       <Head>
-      <title>Docovery | Landing Page</title>
+        <title>Docovery | Landing Page</title>
+        <meta name="title" content="Docovery" />
+        <meta name="description" content="Docovery landing page" />
+        <link rel="icon" href="docovery_logo.svg" />
       </Head>
       <div className={inter.className}>{children}</div>
     </>
-  )
+  );
 }
