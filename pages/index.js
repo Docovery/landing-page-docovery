7b7c42e36c0f969a91e217545f8e70e5a8ff88cb
Banitongwa David <davidbanitongwa@gmail.com>
2023-07-08 19:11:18 +0100

    update layout

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import RootLayout from "./layout"
 
 export default function Home() {
   return (
     <>
-      <div className="mx-auto w-[50%] shadow-md text-center mt-[300px]">
-      <h1>Hello world !</h1>
-      </div>
+      <RootLayout>
+        <div className="mx-auto w-[50%] shadow-md text-center mt-[300px]">
+        <h1>Hello world !</h1>
+        </div>
+      </RootLayout>
     </>
   )
 }
