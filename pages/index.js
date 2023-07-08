bc133168338834e71bc3e2bb40a8c8cf9c15a134
Banitongwa David <davidbanitongwa@gmail.com>
2023-07-08 12:09:25 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
+      <div className="mx-auto w-[50%] shadow-md text-center mt-[300px]">
       <h1>Hello world !</h1>
+      </div>
     </>
   )
 }
