39242a458d7eaf72bcfcd87380fb220694ec9bc4
Banitongwa David <davidbanitongwa@gmail.com>
2023-07-20 20:00:52 +0100

    update team

--- components/team/Team.jsx
+++ components/team/Team.jsx
@@ -6,13 +6,13 @@ export default function Team() {
     <div className="container cont-team">
       <div className="row team-items">
         <h2 className="mb-5 text-center" data-aos="fade-up">
-          L'équipe <strong>Docovery</strong>
+          {"L'équipe"} <strong>Docovery</strong>
         </h2>
         <div className="col-lg-3 col-md-6 col-sm-6 mt-80">
           <div className="card bg-white  d-flex align-items-center justify-content-center ">
             <div className="w-100">
               <Image
-                src="https://images.pexels.com/photos/7467850/pexels-photo-7467850.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                src="avatar.jpg"
                 alt=""
                 width={500}
                 height={500}
@@ -20,17 +20,14 @@ export default function Team() {
               />
             </div>
             <div className="text-center ">
-              <p className="name">Jason Smith</p>
-              <p className="job">ENGINE MECHANIC</p>
+              <p className="name">David BANITONGWA</p>
+              <p className="job">Software Engineer & Co-founder Docovery</p>
               <ul className="d-flex align-items-center justify-content-center list-unstyled icons">
-                <li className="icon ">
+                <li className="icon">
                   <span className="bi bi-twitter"></span>
                 </li>
                 <li className="icon mx-2">
                   <span className="bi bi-facebook"></span>
-                </li>
-                <li className="icon me-2">
-                  <span className="bi bi-google-plus"></span>
                 </li>
                 <li className="icon ">
                   <span className="bi bi-instagram"></span>
@@ -47,7 +44,7 @@ export default function Team() {
           <div className="card bg-white  d-flex align-items-center justify-content-center">
             <div className="w-100">
               <Image
-                src="https://images.pexels.com/photos/7467850/pexels-photo-7467850.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                src="avatar.jpg"
                 alt=""
                 width={500}
                 height={500}
@@ -55,87 +52,14 @@ export default function Team() {
               />
             </div>
             <div className="text-center ">
-              <p className="name">Jason Smith</p>
-              <p className="job">ENGINE MECHANIC</p>
+              <p className="name">Omari KAYUMBA</p>
+              <p className="job">Software Engineer & Co-founder Docovery</p>
               <ul className="d-flex align-items-center justify-content-center list-unstyled icons">
-                <li className="icon ">
+                <li className="icon">
                   <span className="bi bi-twitter"></span>
                 </li>
                 <li className="icon mx-2">
                   <span className="bi bi-facebook"></span>
-                </li>
-                <li className="icon me-2">
-                  <span className="bi bi-google-plus"></span>
-                </li>
-                <li className="icon ">
-                  <span className="bi bi-instagram"></span>
-                </li>
-              </ul>
-              <p className="dis pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Consequatur.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 col-sm-6 mt-80">
-          <div className="card bg-white  d-flex align-items-center justify-content-center">
-            <div className="w-100">
-              <Image
-                src="https://images.pexels.com/photos/7467850/pexels-photo-7467850.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                alt=""
-                width={500}
-                height={500}
-                className="rounded-circle"
-              />
-            </div>
-            <div className="text-center ">
-              <p className="name">Jason Smith</p>
-              <p className="job">ENGINE MECHANIC</p>
-              <ul className="d-flex align-items-center justify-content-center list-unstyled icons">
-                <li className="icon ">
-                  <span className="bi bi-twitter"></span>
-                </li>
-                <li className="icon mx-2">
-                  <span className="bi bi-facebook"></span>
-                </li>
-                <li className="icon me-2">
-                  <span className="bi bi-google-plus"></span>
-                </li>
-                <li className="icon ">
-                  <span className="bi bi-instagram"></span>
-                </li>
-              </ul>
-              <p className="dis pb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Consequatur.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 col-sm-6 mt-80">
-          <div className="card bg-white  d-flex align-items-center justify-content-center">
-            <div className="w-100">
-              <Image
-                src="https://images.pexels.com/photos/7467850/pexels-photo-7467850.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                alt=""
-                width={500}
-                height={500}
-                className="rounded-circle"
-              />
-            </div>
-            <div className="text-center ">
-              <p className="name">Jason Smith</p>
-              <p className="job">ENGINE MECHANIC</p>
-              <ul className="d-flex align-items-center justify-content-center list-unstyled icons">
-                <li className="icon ">
-                  <span className="bi bi-twitter"></span>
-                </li>
-                <li className="icon mx-2">
-                  <span className="bi bi-facebook"></span>
-                </li>
-                <li className="icon me-2">
-                  <span className="bi bi-google-plus"></span>
                 </li>
                 <li className="icon ">
                   <span className="bi bi-instagram"></span>
