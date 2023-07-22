4994fdd9a7db9e316989c5ff2c28689d3e6a6304
Banitongwa David <davidbanitongwa@gmail.com>
2023-07-22 13:52:03 +0100

    update links

--- utils/socialTeam.js
+++ utils/socialTeam.js
@@ -2,7 +2,7 @@ const socialTeam = {
     "omari": {
         "linkedin": "https://www.linkedin.com/in/omari-kayumba-2973a420b/",
         "github": "https://github.com/Rimao416",
-        "twitter": "#"
+        "twitter": "https://twitter.com/rimao416"
     },
     "david": {
         "linkedin": "https://www.linkedin.com/in/david-banitongwa/",
