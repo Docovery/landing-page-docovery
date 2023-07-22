18a9d458e608e5f06edce105793a570dd890ff48
Banitongwa David <davidbanitongwa@gmail.com>
2023-07-22 14:21:03 +0100

    Fix testimonial component

--- components/testimonials/Testimonials.jsx
+++ components/testimonials/Testimonials.jsx
@@ -10,8 +10,8 @@ export default function Testimonials() {
                <div className="row">
 
                     <div className="col-lg-6 col-md-5 col-12">
-                        <div className="contact-image" data-aos="fade-up">
-                          <Image src="/male-avatar.png" className="img-fluid" alt="website" width={916} height={836} />
+                        <div className="client-image" data-aos="fade-up">
+                          <Image src="/male-avatar.png" className="img-fluid w-75 h-75" alt="website" width={916} height={836} />
                         </div>
                     </div>
 
@@ -20,7 +20,7 @@ export default function Testimonials() {
 
                       <div className="quote" data-aos="fade-up" data-aos-delay="200"></div>
 
-                      <h2 className="mb-4" data-aos="fade-up" data-aos-delay="300">{"Je suis absolument ravi de l'application développée par Docovery ! C'est exactement ce que je cherchais. L'interface est conviviale et intuitive, rendant l'utilisation de l'application simple et agréable. Encore une fois, un grand merci pour cette expérience client remarquable !"}</h2>
+                      <h5 className="mb-4" data-aos="fade-up" data-aos-delay="300">{"Je suis absolument ravi de l'application développée par Docovery ! C'est exactement ce que je cherchais. L'interface est conviviale et intuitive, rendant l'utilisation de l'application simple et agréable. Encore une fois, un grand merci pour cette expérience client remarquable !"}</h5>
 
                       <p data-aos="fade-up" data-aos-delay="400">
                         <strong>John Doe</strong>
