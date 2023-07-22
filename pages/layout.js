60eb1606ab478f65a6b064cf518562c7b3b6955a
Banitongwa David <davidbanitongwa@gmail.com>
2023-07-22 14:28:53 +0100

    Update metatag

--- pages/layout.js
+++ pages/layout.js
@@ -9,9 +9,22 @@ export default function RootLayout({ children }) {
       <Head>
         <title>Docovery | Landing Page</title>
         <meta name="title" content="Docovery" />
-        <meta name="description" content="Docovery landing page" />
+        <meta name="description" content="Building solutions, Shaping the future" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="docovery_logo.svg" />
+        <link rel="icon" href="/docovery_logo.svg" />
+        <link rel="icon" type="image/x-icon" href="/docovery_logo.svg" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/docovery_logo.svg" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://docovery.netlify.app" />
+        <meta property="og:title" content="Docovery" />
+        <meta property="og:description" content="Building solutions, Shaping the future" />
+        <meta property="og:image" content="/docovery_logo.svg" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://docovery.netlify.app" />
+        <meta property="twitter:title" content="Docovery" />
+        <meta property="twitter:description" content="Building solutions, Shaping the future" />
+        <meta property="twitter:image" content="/docovery_logo.svg" />
       </Head>
       <div className={inter.className}>{children}</div>
     </>
